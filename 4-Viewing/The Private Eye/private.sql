b22104786921dd53aca9b5a5a984325cff21641b
Hossam Hatem <7ossam7atem1@gmail.com>
2024-10-28 21:39:19 +0300

    fix: secret message error in pset4

--- 4-Viewing/The Private Eye/private.sql
+++ 4-Viewing/The Private Eye/private.sql
@@ -1,24 +1,26 @@
-CREATE TABLE "temp" (
-    "sentence_id" INTEGER,
-    "character_number" INTEGER,
-    "length" INTEGER
-);
+CREATE TABLE
+	triplets (
+		sentence_number INTEGER,
+		start_char      INTEGER,
+		length          INTEGER
+	);
 
-INSERT INTO "temp" ("sentence_id", "character_number", "length")
+INSERT INTO
+	triplets (sentence_number, start_char, length)
 VALUES
-    (14, 98, 4),
-    (114, 3, 5),
-    (618, 72, 9),
-    (630, 7, 3),
-    (932, 12, 5),
-    (2230, 0, 7),
-    (2346, 4, 10),
-    (3041, 4, 5);
+	(14, 98, 4),
+	(114, 3, 5),
+	(618, 72, 9),
+	(630, 7, 3),
+	(932, 12, 5),
+	(2230, 50, 7),
+	(2346, 44, 10),
+	(3041, 14, 5);
 
-CREATE VIEW "message" AS
-    SELECT SUBSTR("sentence", "character_number", "length") AS "phrase"
-    FROM "sentences"
-    JOIN "temp" ON
-        "sentences"."id" = "temp"."sentence_id";
-
-SELECT "phrase" FROM "message";
+CREATE VIEW
+	message AS
+SELECT
+	substr (s.sentence, t.start_char, t.length) AS phrase
+FROM
+	sentences AS s
+	JOIN triplets AS t ON t.sentence_number = s.id;
